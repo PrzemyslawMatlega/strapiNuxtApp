6ee5ee55cdc22c87c8363d1d92fc2221495ac6aa
Przemek Matlęga <przemyslaw.matlega@1000i.pl>
2020-01-27 13:20:16 +0100

    Ordering the project and creating new clean version

---
+++ frontendClean/components/ArticleSelectComponents/Galeria.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h4>galeria</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ frontendClean/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+        <br>
+        <hr>
+        <h3>Articles Select</h3>
+        <hr>
+        <ArticlesSelect :articles="artykulSelects"/>
+ 
+  </div>
+</template>
+
+<script>
+  import ArticlesSelect from '~/components/ArticlesSelect'
+
+  export default {
+    data() {
+      return {
+  
+      }
+    },
+    asyncData(context){
+          return fetch('http://localhost:1337/artykul-selects')
+        .then(response => response.json())
+        .then(myResponse => {
+          const responseArr =[]
+          myResponse.forEach( singlePost => {
+            responseArr.push(singlePost)
+          })
+          console.log(responseArr)
+
+          return{
+            artykulSelects:responseArr
+          }
+        })
+    },
+    components: {
+      ArticlesSelect,
+    },
+  
+  }
+
+</script>
